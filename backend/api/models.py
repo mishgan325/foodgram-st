30134739719852476a861eaf24eb6e8bbb957089
Michael <shibut.m.v@edu.mirea.ru>
2025-05-25 17:18:15 +0300

    Add ShoppingCart, Favorite

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.conf import settings
+from recipes.models import Recipe
 
-# Create your models here.
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='shopping_cart_items',
+        verbose_name="Пользователь"
+    )
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='user_shopping_cart',
+        verbose_name="Выбранный рецепт"
+    )
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_user_recipe_shopping_cart'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} выбрал ({self.measurement_unit})"
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name="Пользователь"
+    )
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorite_recipies',
+        verbose_name="Избранный рецепт"
+    )
+
+    class Meta:
+        verbose_name = "Избранное"
+        verbose_name_plural = "Избранные"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_user_recipe_favorites'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} оценил ({self.measurement_unit})"
