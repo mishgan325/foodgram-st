d63e69ae5d4a259706eda2e9b5e0152be6a9b35b
Michael <shibut.m.v@edu.mirea.ru>
2025-05-26 20:23:00 +0300

    Setup admin zone

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+    min_num = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "cooking_time", "favorites_count")
+    search_fields = ("name", "author__username", "author__first_name", "author__last_name")
+    list_filter = ("author",)
+    ordering = ("name",)
+    inlines = [RecipeIngredientInline]
+
+    @admin.display(description="В избранном у пользователей")
+    def favorites_count(self, recipe):
+        return recipe.favorite_recipes.count()
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "measurement_unit", "recipe_count")
+    search_fields = ("name",)
+    list_filter = ("measurement_unit", )
+    ordering = ("name",)
+
+    @admin.display(description="Число рецептов")
+    def recipe_count(self, ingredient):
+        return ingredient.recipe_ingredients.count()
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Subscription
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    search_fields = ('first_name', 'email')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('username',)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('subscriber', 'author')
+    search_fields = ('subscriber__username', 'author__username')
